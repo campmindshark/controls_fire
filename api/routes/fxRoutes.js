5b9ce2fcf8969165fb894c4a96ed057d1cf238a4
Tushar Shah <twoshark@gmail.com>
2017-12-22 20:22:42 +0000

    framing api

---
+++ api/routes/fxRoutes.js
@@ -0,0 +1,15 @@
+use 'strict';
+module.exports = function(app) {
+	var fxs = require('../controllers/fxController');
+
+	//Direct Effect Control Routes
+	app.route('/fxs')
+		.get(fxs.list_all_fxs) 
+		.post(fxs.enable_fx)
+		.delete(fxs.disable_fxs);
+
+	app.route('/fxs/:fxId')
+		.get(fxs.get_fx_details)
+		.post(fxs.set_fx_state)
+		.delete(fxs.disable_fx);		
+}
