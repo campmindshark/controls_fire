4ae23a032a026ccffb75b3c069baefa79cbde14a
tushar <tushar@gravitasvt.con>
2018-01-02 22:51:57 -0500

    removing spaces in configuration keys

--- config.js
+++ config.js
@@ -3,24 +3,24 @@ var appConfig = {
 	"version":"1.0.0"
 	"parts": {
 		supplies: {
-			"Example Supply Name": ["Manifold 1", "Manifold2"]
+			"example_supply_name": ["manifold_line_1", "manifold_line_2"]
 			"why": ["W1", "W2", "W3", "H4", "H5", "Y6", "Y7"],
-			"left eye": [],
-			"right eye": [],
-			"master":["why", "left eye", "right eye"]
+			"left_eye": [],
+			"right_eye": [],
+			"master":["why", "left_eye", "right_eye"]
 		}
 		effects: {
-			"Example Effect Name":["Supply Solenoid 1", "Supply Solenoid 2" ]	
+			"example_effect_name":["supply_solenoid_1", "supply_solenoid_2" ]	
 			"fx1":["why"],
-			"fx2":["why", "right eye"],
-			"fx3":["left eye"],
+			"fx2":["why", "right_eye"],
+			"fx3":["left_eye"],
 			"fx4":["W1"],
 			"W":["W1", "W2", "W3"],
 			"H":["H4", "H5"],
 			"Y":["Y6", "Y7"]	
 		}
 		igniters: {
-			"Example Igniter": ["Effect 1","Effect 2","Effect 3"],
+			"example_igniter": ["fx1","fx2","fx3"],
 			"W1":["W"],
 			"W2":["W"],
 			"W3":["W"],
