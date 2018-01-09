5f5ed8eba557a748add91786d36d2e5511201b50
tushar <tushar@gravitasvt.con>
2018-01-09 18:32:49 -0500

    cors middleware

--- server.js
+++ server.js
@@ -4,15 +4,6 @@ var express = require('express'),
 	bodyParser = require('body-parser'),
 	cors = require('cors');
 
-
-
-//CORS middleware
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-                }
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
