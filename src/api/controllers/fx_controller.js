ca38b40e785994f06ad9aceefd9943337e27bd8f
tushar <tushar@gravitasvt.con>
2018-01-31 22:13:36 -0500

    accept part name or gpio id to command parts

--- src/api/controllers/fx_controller.js
+++ src/api/controllers/fx_controller.js
@@ -17,30 +17,40 @@ exports.enable_fx = function(req,res) {
 	//POST /fxs
 	//Request to enable client control of req.params.fxId
 	//console.log(req);
-	console.log('\nRequest to enable fxId: ' + req.body.fxId + '\n' + Date.now() );
-	var msg = handle_api_call(req.app.locals.effects.enable_effect(req.body.fxId)
+	console.log('\nRequest to enable fxId: ' + req.body.fxId + '\n' + Date.now());
+	//handle either gpio id or part.name
+	var id = req.app.locals.effects.id_check(req.body.fxId);
+	if (id != "bad id") {
+		var msg = handle_api_call(req.app.locals.effects.enable_effect(id)
 		, 'Enabled fxId: ' + req.body.fxId + '\n'
 		, 'Request to Enable fxId ' + req.body.fxId + '\n');
-	res.send(msg);
+		res.send(msg);
+	} else {
+		res.send('Bad Id');
+	}
 }
 
 exports.disable_fxs = function(req,res) {
 	//DELETE /fxs
 	//turn EVERYTHING off. Disable all fx.
 	console.log('Master Shut Off Request' + '\n' + Date.now());
-	var msg = handle_api_call(req.app.locals.effects.master_shut_off()
-		,"Master Shut Off Request Completed"
-		,"Master Shut Off Request Did Not Complete")
+	var msg = handle_api_call(req.app.locals.effects.master_shut_off(true)
+	,"Master Shut Off Request Completed"
+	,"Master Shut Off Request Did Not Complete")
 	res.send(msg);
-
 }
 
 exports.get_fx_details = function(req,res) {
 	//GET /fx/:fxId
 	//type and state informatiom for req.params.fxId
 	console.log('Details request for fxId:'+  req.params.fxId);
-	var msg = req.app.locals.effects.get_effect_details(req.params.fxId);
-	res.send(msg);
+	var id = req.app.locals.effects.id_check(req.params.fxId);
+	if (id != 'bad id') {
+		var msg = req.app.locals.effects.get_effect_details(id);
+		res.send(msg);
+	} else {
+		res.send('Bad Id');
+	}
 }
 
 exports.set_fx_state = function(req,res) {
@@ -48,24 +58,35 @@ exports.set_fx_state = function(req,res) {
 	//change state of req.params.fxId to req.params.fxState
 	console.log('\nState change requested for fxId: ' + req.params.fxId);
 	console.log('\nNew State Requested: ' + req.body.fxState);
-	var msg = handle_api_call(
-		req.app.locals.effects.command_effect(req.params.fxId, req.body.fxState)
-		, "\nEffect command completed."
-		, "\nEffect command did not complete"
-	);
-	res.send(msg);
+	var id = req.app.locals.effects.id_check(req.params.fxId);
+	if (id != 'bad id') {
+		var msg = handle_api_call(
+			req.app.locals.effects.command_effect(id, req.body.fxState)
+			, "\nEffect command completed."
+			, "\nEffect command did not complete"
+		);
+		res.send(msg);
+	} else {
+		res.send('Bad Id');
+	}
 }
 
 exports.disable_fx = function(req,res){
 	//DELETE /fxs/:fx
 	//Turn off and disable client control of req.params.fxId
 	console.log('Disable request for fxId: ' + req.params.fxId);
-	var msg = handle_api_call(
-		req.app.locals.effects.disable_effect(req.params.fxId)
-		,"\nEffect ID: " + req.params.fxId + " has been Disabled."
-		,"\nEffect ID: " + req.params.fxId + " could not been Disabled."
- 	)
-	res.send(msg);
+	var id = req.app.locals.effects.id_check(req.params.fxId);
+	if (id != 'bad id') {
+		var msg = handle_api_call(
+			req.app.locals.effects.disable_effect(id, false)
+			,"\nEffect ID: " + req.params.fxId + " has been Disabled."
+			,"\nEffect ID: " + req.params.fxId + " could not been Disabled."
+		)
+		res.send(msg);
+	} else {
+		res.send('Bad Id');
+	}
+
 }
 
 const handle_api_call = function(method_call, success_msg, fail_msg,) {
--- src/api/models/fx_model.js
+++ src/api/models/fx_model.js
@@ -3,26 +3,36 @@ import gpio from "./gpio_model";
 
 export default class Effects {
   constructor(json_config) {
-
     var config = JSON.parse(json_config);
 
     this.name = config.name;
     this.version = config.version;
-
-		this.effect_array = this.build_array(config.parts);
+    this.effect_array = this.build_array(config.parts);
   }
 
   build_array = function(parts) {
-    
-    if (parts.length > gpio.gpio_pins.length) {
-      throw "TOO MANY EFFECTS.";
+
+    if (parts!= null && parts.length > 0) {
+      if (parts.length > gpio.gpio_pins.length) {
+        throw "TOO MANY PARTS. Configure fewer parts.";
+      }
+      var fxs = [];
+      for (var i = 0; i< parts.length; i++) {
+        //TODO: do something with the different input types
+        //TODO: load initial values from config
+        fxs[i] =
+        {
+            "part" : parts[i],
+            "gpio" : "init"
+        };
+      }
+      return fxs;
+    } else {
+      throw "No Parts Configured.\nYou must configure parts to control fire.";
     }
-
-    console.log('parts: ' + JSON.stringify(parts));
-
   }
 
-  build_demo_array = function() {
+   build_demo_array = function() {
     //just a dummy array of 12 effects
     var fxs = [];
     for(var i=0; i<12; i++) {
@@ -40,39 +50,44 @@ export default class Effects {
     return JSON.stringify(this.effect_array);
   }
 
-	command_effect = function(id, state) {
-		if(this.effect_array[id] != "Disabled") {
-		    if (this.set_effect_state(id, state)) {
-          console.log("New effect state set");
-          return true;
-        } else {
-          console.log("Failed to set new effect state");
-          return false;
-        }
+  get_effect_details = function(id) {
+    return JSON.stringify(this.effect_array[id]);
+  }
+
+  command_effect = function(id, state) {
+    if(this.effect_array[id].gpio != "Disabled") {
+      if (this.set_effect_state(id, state)) {
+        console.log("New effect state set");
+        return true;
+      } else {
+        console.log("Failed to set new effect state");
+        return false;
+      }
     } else {
       console.log("Cannot Command Disabled Effect ID: " + id);
       return false;
     }
-	}
+  }
 
-	set_effect_state = function(id, state) {
+  set_effect_state = function(id, state) {
     try {
-        //check for valid state
-        if (!(state == 1 || state == 0 )) {
-          throw "\ninvalid state:" + state + "\nfor id: " + id;
-        }
-		    this.effect_array[id].Value = state;
-		      return true;
+      //check for valid state
+      if (!(state == 1 || state == 0 )) {
+        throw "\ninvalid state:" + state + "\nfor id: " + id;
+      }
+      this.effect_array[id].gpio.Value = state;
+      return true;
     }
     catch(err) {
       console.log(err);
       return false;
     }
-	}
+  }
 
   enable_effect = function(id) {
     try {
-      this.effect_array[id] = new gpio(id, this.mode_test(), 0);
+      this.effect_array[id].gpio = new gpio(id, this.mode_test(), 0);
+      console.log (this);
       return true;
     }
     catch(err) {
@@ -83,9 +98,9 @@ export default class Effects {
   disable_effect = function(id, graceful) {
     try {
       if (graceful) {
-        this.effect_array[id].Value = 0;
+        this.effect_array[id].gpio.Value = 0;
       }
-      this.effect_array[id] = "Disabled";
+      this.effect_array[id].gpio = "Disabled";
       return true;
     }
     catch(err) {
@@ -108,10 +123,26 @@ export default class Effects {
     }
   }
 
-	mode_test = function() {
+  mode_test = function() {
     //TODO: add Beaglebone black mode
-		//only mocking gpio work for now
-		return "mock";
-	}
+    //only mocking gpio work for now
+    return "mock";
+  }
 
+  id_check = function(test_id) {
+    //TODO: handle the ids better. a db would probably help.
+  	if (typeof !isNaN(parseInt(test_id)) && isFinite(test_id)) {
+  		if (test_id < this.effect_array.length) {
+  			return test_id;
+  		}
+  		else {
+  			return "bad id";
+  		}
+  	}
+    for(var i = 0; i<this.effect_array.length; i++) {
+      if (this.effect_array[i].part.name == test_id) {
+        return i;
+      }
+    }
+  }
 }
