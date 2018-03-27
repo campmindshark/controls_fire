ef37f2336833c1465181b53a8e9d11607be0bb99
tushar <tushar@gravitasvt.con>
2018-03-27 00:23:42 -0400

    cors fix

--- server/server.js
+++ server/server.js
@@ -17,11 +17,33 @@ const app = express(),
   indexPath = path.join(__dirname, "../hot_and_gui/index.html");
 
 app.use(express.static(staticPath));
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(indexPath);
 });
 
-app.use(cors());
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(
   bodyParser.urlencoded({
     extended: true
